2d73e3b47e432d9dc4d676fe09dd8514339c19e3
Ansh-Gautam <97623233+Ansh-Gautam@users.noreply.github.com>
2023-05-07 11:43:35 +0530

    Update unfollow_who_not_followyou.py

--- unfollow_who_not_followyou.py
+++ unfollow_who_not_followyou.py
@@ -1,27 +1,9 @@
-'''import random
-from instagrapi import Client
-
-password = 'goodansh2000'
-username = 'an_us_kha'
-client = Client()
-client.login(username,password)
-
-followers = client.user_followers(client.user_id)
-following = client.user_following(client.user_id)
-
-key_to_unfollow = set(following.keys()).difference(followers.keys())
-for key in key_to_unfollow:
-	userid = following[key].pk
-	name = following[key].username
-	print(f"Unfollowed : {name}")
-	client.user_unfollow(userid)
-'''
 import random
 import time
 from instagrapi import Client
 
-password = 'goodansh2000'
-username = 'an_us_kha'
+password = 'password'
+username = 'username'
 client = Client()
 client.login(username, password)
 
