4dadd97833fb042b6f2d79da2feaf4d6e0600d14
Ansh-Gautam <97623233+Ansh-Gautam@users.noreply.github.com>
2023-05-07 11:42:17 +0530

    Update likebyhastag.py

--- likebyhastag.py
+++ likebyhastag.py
@@ -1,34 +1,12 @@
 import random
 from instagrapi import Client
 
-password = 'goodansh2000'
-username = 'an_us_kha'
+password = 'password'
+username = 'username'
 client = Client()
 client.login(username,password)
 hashtag = "selfielove"
-#comments = ["Great Post","Blissful Post","Happy for you","Its Wonderful"]
-comments = [
-    "Looking gorgeous! ❤️",
-    "You are absolutely stunning! 😍",
-    "Love the selfie, you are a natural beauty! 🌸",
-    "Your smile is so infectious! 😊",
-    "Beautiful picture! 🔥",
-    "Love your confidence, keep shining! ✨",
-    "You're so photogenic! 😘",
-    "Slaying it with this selfie! 🔥",
-    "Gorgeous shot! 📸",
-    "You look amazing, keep slaying! 💕",
-    "Absolutely stunning! 😍",
-    "Beautiful selfie, you have such a great smile! 😊",
-    "Love the picture, you are a natural beauty! 🌸",
-    "You're a natural in front of the camera! 😘",
-    "Flawless shot! 🔥",
-    "This picture is amazing, love your style! ✨",
-    "You are stunning, keep shining! 💕",
-    "Great selfie, you have such a lovely personality that shines through! 🌟",
-    "Love the selfie, you are rocking it! 🙌",
-    "Incredible shot, you are a true beauty! 😍"
-]
+comments = ["Great Post","Blissful Post","Happy for you","Its Wonderful"]
 
 medias = client.hashtag_medias_recent(hashtag,100)
 
